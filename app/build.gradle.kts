6eac3f0ac77df350291b7cad3b8cee1c823f2584
Katham28 <carpiodkm@gmail.com>
2026-09-16 22:42:18 -0600

    si

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.dependencies
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -39,6 +41,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
@@ -46,6 +49,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.navigation.compose)
+    implementation (libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
@@ -54,4 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
 }
--- app/src/main/java/com/pruebas/practica2/MainActivity.kt
+++ app/src/main/java/com/pruebas/practica2/MainActivity.kt
@@ -7,22 +7,33 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+
 import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedSecureTextField
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -37,11 +48,80 @@ import com.pruebas.practica2.ui.theme.Practica2Theme
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil3.compose.AsyncImage
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
 
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.converter.gson.GsonConverterFactory
+import java.security.MessageDigest
+
+//Envoltura o wrapper para el digest sha256
+fun sha256(text:String): String
+{
+    val bytes= MessageDigest.getInstance("SHA-256").digest(text.toByteArray())
+    return bytes.joinToString(separator = "") { "%02X".format(it.toInt() and 0xFF) }
+}
+
+
+data class DatoPerturbador(
+    val id: Int,
+    val text: String,
+    val source:String?
+    )
+
+data class LoginResponse(
+    val username:String,
+    val id: Int,
+    val email:String,
+    val firstname:String,
+    val lastname:String,
+    val pfp_url: String,
+    val credits : Int,
+    val xp: Int,
+    val error: String?
+)
+
+
+val colores = listOf(
+    Color(0xFF5D3140),
+    Color(0xFFCF4173),
+    Color(0xFFF39399),
+    Color(0xFFF6D8BD)
+)
+
+interface ApiService {
+    @GET("datoperturbador")
+    suspend fun getDatoPerturbador(): DatoPerturbador
+    
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field (value = "user") user: String,
+        @Field (value = "pass") pass: String,
+        @Field (value = "token") token: String,
+    ): LoginResponse
+}
+
+//instancia de retrofir que prepara y hace las peticiones
+object RetrofitInstance{
+    val api: ApiService by lazy{
+        Retrofit.Builder()
+            .baseUrl("https://monsterballgo.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,31 +139,104 @@ class MainActivity : ComponentActivity() {
 fun App()
 {
     val navController = rememberNavController()
-
+    var userData by remember { mutableStateOf<LoginResponse?>(value = null) }
     NavHost(
         navController = navController,
-        startDestination = "mainmenu"
+        startDestination = "mainmenu",
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colores[3])
     )
     {
         composable("mainmenu")
         {
-            MainMenu(   {
-                Log.d("MyApp", "boton de login presionado")
-                navController.navigate("home")
-
-            }    )
+            MainMenu { ud ->
+                userData = ud
+                navController.navigate(route = "home")
+            }
         }
         composable("home")
         {
-            Home()
+            val user=userData
+            if (user ==null){
+                //o se puede llegar a home sin userDATA
+                LaunchedEffect(key1 = Unit){
+                     navController.navigate(route="mainmenu")
+                }
+            }else{
+
+                Home (
+                    userData = user,
+                    onLogout = {
+                        userData = null
+                        navController.navigate(route = "mainmenu")
+                    },
+                    onCreditos = {
+                        navController.navigate(route = "creditos")
+                    } ,
+                    onPerfil = {
+                        navController.navigate(route = "perfil")
+                    }
+                )
+            }
+
         }
+
+        composable("creditos")
+        {
+            val user=userData
+            if (user ==null){
+                LaunchedEffect(key1 = Unit){
+                     navController.navigate(route="mainmenu")
+                }
+            }else{
+                Creditos(
+                    userData = user,
+                    onHome = {
+                        navController.navigate(route = "home")
+                    },
+                    onPerfil = {
+                        navController.navigate(route = "perfil")
+                    },
+                    onLogout = {
+                        userData = null
+                        navController.navigate(route = "mainmenu")
+                    }
+                )
+            }
+        }
+
+        composable("perfil")
+        {
+            val user=userData
+            if (user ==null){
+                LaunchedEffect(key1 = Unit){
+                    navController.navigate(route="mainmenu")
+                }
+            }else{
+                Perfil(
+                    userData = user,
+                    onHome = {
+                        navController.navigate(route = "home")
+                    },
+                    onCreditos = {
+                        navController.navigate(route = "creditos")
+                    },
+                    onLogout = {
+                        userData = null
+                        navController.navigate(route = "mainmenu")
+                    }
+                )
+            }
+        }
+
     }
 
 }
 
 @Composable
 //recibe como parametro un callback
-fun MainMenu( onLoginSuccess: ()->Unit  )
+fun MainMenu( onLoginSuccess: (LoginResponse)->Unit  )
 {
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -134,10 +287,10 @@ fun MainMenu( onLoginSuccess: ()->Unit  )
 }
 
 @Composable
-fun Login(   onLoginSuccess: ()->Unit   )
+fun Login(   onLoginSuccess: (LoginResponse)->Unit   )
 {
     val scope = rememberCoroutineScope()
-
+    var textoerror by remember {mutableStateOf(value = "")}
     Column(
         modifier = Modifier.fillMaxWidth(0.9f)
             .padding(24.dp),
@@ -157,23 +310,175 @@ fun Login(   onLoginSuccess: ()->Unit   )
             label = { Text("password") }
         )
 
+
+        var  buttonEnabledState by remember {mutableStateOf(value= true)}
         ///botonazo de login
         Button (
+            enabled = buttonEnabledState,
             onClick = {
-                onLoginSuccess()
+                buttonEnabledState = false
+                scope.launch{
+                    try{
+                        val userData= RetrofitInstance.api.login(
+                            user=usernameState.text.toString(),
+                            pass= sha256(text = passState.text.toString()),
+                            token = "code37"
+                        )
+                        if (userData.error!=null){
+                            Log.d("Myapp","error: ${userData.error}")
+                            buttonEnabledState = true
+                            textoerror="Usuario o contraseña incorrectos"
+                        } else {
+                            textoerror=""
+                            Log.d("Myapp","usuario logueado: ${userData.username}")
+                            onLoginSuccess(userData)
+                        }
+
+
+                    } catch(error: Exception){
+                        Log.d("Myapp","Excepción en login: ${error.message}")
+                        buttonEnabledState = true
+                        textoerror="Error al iniciar sesión, intentelo más tarde"
+                    }
+                }
                 //onLoginSucces( respuesta del servidor porcesada)
             }
         )
         {
             Text("Log in!")
         }
+        Text(
+            fontSize = 26.sp,
+            text = textoerror,
+            color = Color.Red
+        )
     }
 
 }
+@Composable
+fun BarraTOP(
+    onLogout: () -> Unit
+)
+{
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+
+
+        Button (
+            colors = ButtonDefaults.buttonColors(containerColor = colores[1]),
+            onClick = { onLogout() }
+        )
+        {
+            Icon(
+                imageVector = Icons.Default.ExitToApp,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Cerrar sesión")
+        }
+    }
+}
+
 
 @Composable
-fun Home()
+fun BarraNavegacion(
+    pantallaActual: String,
+    onHome: () -> Unit,
+    onCreditos: () -> Unit,
+    onPerfil: () -> Unit,
+)
 {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Button (
+            colors = ButtonDefaults.buttonColors(containerColor = colores[2]),
+            enabled = pantallaActual != "home",
+            onClick = { onHome() }
+        )
+        {
+            Icon(
+                imageVector = Icons.Default.Home,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Home")
+        }
+
+        Button (
+            colors = ButtonDefaults.buttonColors(containerColor = colores[2]),
+            enabled = pantallaActual != "creditos",
+            onClick = { onCreditos() }
+        )
+        {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Créditos")
+        }
+
+        Button (
+            colors = ButtonDefaults.buttonColors(containerColor = colores[2]),
+            enabled = pantallaActual != "perfil",
+            onClick = { onPerfil() }
+        )
+        {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize)
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text("Perfil")
+        }
+
+    }
+}
+
+@Composable
+fun Home(
+    userData: LoginResponse,
+    onLogout: () -> Unit,
+    onCreditos: () -> Unit,
+    onPerfil: () -> Unit
+)
+{
+
+    val scope=rememberCoroutineScope ()
+    var textoDato by remember {mutableStateOf(value = "obteniendo dato...")}
+
+    var cargandoDato by remember { mutableStateOf(value = true) }
+
+    LaunchedEffect (key1 = Unit)
+    {
+        try{
+            val res = RetrofitInstance.api.getDatoPerturbador()
+            textoDato= res.text
+        }catch(e: Exception){
+            Log.d("MyApp", "error: ${e.message}")
+        } finally {
+            cargandoDato = false
+        }
+    }
+
+    BarraTOP(
+        onLogout = onLogout
+    )
+
+
+
     Column(
         modifier=Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -181,19 +486,134 @@ fun Home()
     ){
         Text(
             fontSize = 28.sp,
-            text = "menu de home"
+            text = "Te damos la bienvenida,${userData.username}"
         )
         Spacer( modifier = Modifier.size(20.dp))
+
+        AsyncImage(
+            modifier = Modifier.size(300.dp),
+            model= userData.pfp_url,
+            //model = "https://cataas.com/cat?type=square",
+            contentDescription="un gato",
+            placeholder = painterResource(R.drawable.reloj_de_arena),
+            error = painterResource(R.drawable.advertencia)
+        )
+
+
         Text(
             fontSize = 14.sp,
-            text = "aqui van a ir cositas"
+            text = textoDato
         )
+
+        BarraNavegacion(
+            pantallaActual = "home",
+            onHome = {},
+            onCreditos = onCreditos,
+            onPerfil = onPerfil
+        )
+
+
     }
 
-    Text(
-        fontSize = 28.sp,
-        text = "menu de home"
+
+}
+
+@Composable
+fun Creditos(
+    userData: LoginResponse,
+    onHome: () -> Unit,
+    onPerfil: () -> Unit,
+    onLogout: () -> Unit
+)
+{
+    val scope=rememberCoroutineScope ()
+
+    BarraTOP(
+        onLogout = onLogout
     )
+
+
+
+
+    Column(
+        modifier=Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            fontSize = 28.sp,
+            text = "Te damos la bienvenida,${userData.username}"
+        )
+        Spacer( modifier = Modifier.size(20.dp))
+
+        Image(
+            modifier = Modifier.size(300.dp),
+            painter = painterResource(R.drawable.advertencia),
+            contentDescription = "un gato"
+        )
+
+        BarraNavegacion(
+            pantallaActual = "creditos",
+            onHome = onHome,
+            onCreditos = {},
+            onPerfil = onPerfil,
+        )
+
+
+
+
+    }
+
+
+}
+
+@Composable
+fun Perfil(
+    userData: LoginResponse,
+    onHome: () -> Unit,
+    onCreditos: () -> Unit,
+    onLogout: () -> Unit
+)
+{
+    val scope=rememberCoroutineScope ()
+
+    BarraTOP(
+        onLogout = onLogout
+    )
+
+
+
+
+    Column(
+        modifier=Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            fontSize = 28.sp,
+            text = "Te damos la bienvenida,${userData.username}"
+        )
+        Spacer( modifier = Modifier.size(20.dp))
+
+        Image(
+            modifier = Modifier.size(300.dp),
+            painter = painterResource(R.drawable.advertencia),
+            contentDescription = "un gato"
+        )
+
+        BarraNavegacion(
+            pantallaActual = "perfil",
+            onHome = onHome,
+            onCreditos = onCreditos,
+            onPerfil = {},
+        )
+
+
+
+
+    }
+
+
 }
 
 
@@ -210,4 +630,70 @@ fun MainPreview() {
 fun LoginPreview()
 {
     Login( {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview()
+{
+    Home(
+        userData = LoginResponse(
+            username="",
+            id =1,
+            email="usa@gmail.com",
+            firstname =  "",
+            lastname = "",
+            pfp_url = "urlsisisi",
+            credits=100,
+            xp=999,
+            error=null
+        ),
+        onLogout = {},
+        onCreditos = {},
+        onPerfil = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CreditosPreview()
+{
+    Creditos(
+        userData = LoginResponse(
+            username="",
+            id =1,
+            email="usa@gmail.com",
+            firstname =  "",
+            lastname = "",
+            pfp_url = "urlsisisi",
+            credits=100,
+            xp=999,
+            error=null
+        ),
+        onHome = {},
+        onPerfil = {},
+        onLogout = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PerfilPreview()
+{
+    Perfil(
+        userData = LoginResponse(
+            username="",
+            id =1,
+            email="usa@gmail.com",
+            firstname =  "",
+            lastname = "",
+            pfp_url = "urlsisisi",
+            credits=100,
+            xp=999,
+            error=null
+        ),
+        onHome = {},
+        onCreditos = {},
+        onLogout = {}
+    )
 }
